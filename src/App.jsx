b98c3d19b47f1b34f43d2b707f587ff17062e0db
EllenaAmanda <ellenaamanda846@gmail.com>
2024-11-30 17:43:11 +0700

    uploaded URL Cloudinary from input type file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Insert from './component/Insert'
 
 function App() {
   const [labels, setLabels] = useState("")
-  const [imageURL, setImageURL] = useState("")
-  const [caption, setCaption] = useState("No image chosen")
+  const [uploadedURL, setUploadedURL] = useState()
+
+  const handleUpload = async (event) => {
+    const selectedFile = event.target.files[0]; // Access the selected file
+    console.log("event target file", selectedFile);
+
+    // Immediately upload the file using the selected file instead of waiting for `file` state
+    if (selectedFile) {
+      const data = new FormData();
+      data.append("file", selectedFile);
+      data.append("upload_preset", "u8jcxkjv"); //nama preset yg dibuat di cloudinary
+      data.append("cloud_name", "dwqiibgrk"); //cloud name cloudinary
+
+      try {
+        // upload image via API with base URL depends on the cloud name (unique identifier assigned to your Cloudinary account)
+        const res = await fetch(`https://api.cloudinary.com/v1_1/dwqiibgrk/image/upload`, {
+          method: "POST",
+          body: data,
+        });
+
+        const hasil = await res.json();
+        console.log("Upload Response:", hasil);
+        setUploadedURL(hasil.secure_url); // Store the secure URL of the uploaded file
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (uploadedURL) {
+      console.log("Uploaded URL:", uploadedURL);
+    }
+  }, [uploadedURL]); // Run the effect whenever `uploadedURL` changes
+  
+  
+
   return (
     <div className='container'>
       <h1>Image Classification</h1>
@@ -13,10 +48,7 @@ function App() {
       <h2 style={{fontWeight: "normal"}}>Upload image to detect area affected by tidal flood.</h2>
 
       <Insert />
-      <div style={{display: "inline-flex", gap:"24px", marginTop: "24px"}}>
-        <input type='file' accept="image/x-png,image/jpeg" className='attachment' />
-        {/* <p style={{fontStyle: "italic"}}>{caption}</p> */}
-      </div>
+        <input style={{marginTop:"24px"}} type='file' onChange={handleUpload} accept="image/x-png,image/jpeg" className='attachment' />
       
     </div>
   )
